a791b7d2d1c466d7380748ddf24ac5a1d11b131b
bachnguyen0175 <154598570+bachnguyen0175@users.noreply.github.com>
2025-01-22 16:18:21 +0700

    Add files via upload

---
+++ utils/general.py
@@ -0,0 +1,105 @@
+import matplotlib.pyplot as plt
+from pathlib import Path
+import glob
+import re
+import torch
+import datetime
+
+def show_image(img, title=None):
+    """Imshow for Tensor."""
+    
+    #unnormalize 
+    img[0] = img[0] * 0.229
+    img[1] = img[1] * 0.224 
+    img[2] = img[2] * 0.225 
+    img[0] += 0.485 
+    img[1] += 0.456 
+    img[2] += 0.406
+    
+    img = img.numpy().transpose((1, 2, 0))
+    
+    plt.imshow(img)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+def increment_path(path, exist_ok=True, sep=''):
+    # Increment path, i.e. runs/exp --> runs/exp{sep}0, runs/exp{sep}1 etc.
+    path = Path(path)  # os-agnostic
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob.glob(f"{path}{sep}*")  # similar paths
+        matches = [re.search(rf"%s{sep}(\d+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]  # indices
+        n = max(i) + 1 if i else 2  # increment number
+        return f"{path}{sep}{n}"  # update path
+    
+
+def get_caps_from(model, features_tensors, device, vocab):
+    #generate the caption
+    model.eval()
+    with torch.no_grad():
+        features = model.encoder(features_tensors.to(device))
+        caps,alphas = model.decoder.generate_caption(features,vocab=vocab)
+        caption = ' '.join(caps)
+        show_image(features_tensors[0],title=caption)
+    
+    return caps,alphas
+
+#Show attention
+def plot_attention(img, result, attention_plot):
+    #untransform
+    img[0] = img[0] * 0.229
+    img[1] = img[1] * 0.224 
+    img[2] = img[2] * 0.225 
+    img[0] += 0.485 
+    img[1] += 0.456 
+    img[2] += 0.406
+    
+    img = img.numpy().transpose((1, 2, 0))
+    temp_image = img
+
+    fig = plt.figure(figsize=(15, 15))
+
+    len_result = len(result)
+    for l in range(len_result):
+        temp_att = attention_plot[l].reshape(7,7)
+        
+        ax = fig.add_subplot(len_result//2,len_result//2, l+1)
+        ax.set_title(result[l])
+        img = ax.imshow(temp_image)
+        ax.imshow(temp_att, cmap='gray', alpha=0.7, extent=img.get_extent())
+        
+
+    plt.tight_layout()
+    plt.show()
+
+def colorstr(*input):
+    # Colors a string https://en.wikipedia.org/wiki/ANSI_escape_code, i.e.  colorstr('blue', 'hello world')
+    *args, string = input if len(input) > 1 else ('blue', 'bold', input[0])  # color arguments, string
+    colors = {'black': '\033[30m',  # basic colors
+              'red': '\033[31m',
+              'green': '\033[32m',
+              'yellow': '\033[33m',
+              'blue': '\033[34m',
+              'magenta': '\033[35m',
+              'cyan': '\033[36m',
+              'white': '\033[37m',
+              'bright_black': '\033[90m',  # bright colors
+              'bright_red': '\033[91m',
+              'bright_green': '\033[92m',
+              'bright_yellow': '\033[93m',
+              'bright_blue': '\033[94m',
+              'bright_magenta': '\033[95m',
+              'bright_cyan': '\033[96m',
+              'bright_white': '\033[97m',
+              'end': '\033[0m',  # misc
+              'bold': '\033[1m',
+              'underline': '\033[4m'}
+    return ''.join(colors[x] for x in args) + f'{string}' + colors['end']
+
+def date_modified(path=__file__):
+    # return human-readable file modification date, i.e. '2021-3-26'
+    t = datetime.datetime.fromtimestamp(Path(path).stat().st_mtime)
+    return f'{t.year}-{t.month}-{t.day}'
